60f8726fdf4ec787264cd5b5e36f21ffa3f22fee
artyte <artyte@outlook.com>
2022-07-31 19:53:03 +0800

    [ADD] player & 2nd level

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public int buildNum = 1;
+    public string SceneName = "Level 2";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        GameObject collided = collider.gameObject;
+        if (collided.name == "Player")
+        {
+            SceneManager.LoadScene(SceneName);
+        }
+    }
+}
